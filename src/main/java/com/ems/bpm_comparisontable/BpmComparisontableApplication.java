c2ce15653694be163e7d07fbc6e1c7ab8c86572a
Wu <jingtingwu@gmail.com>
2025-10-10 22:40:10 +0800

    no message

---
+++ src/main/java/com/ems/bpm_comparisontable/BpmComparisontableApplication.java
@@ -0,0 +1,13 @@
+package com.ems.bpm_comparisontable;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BpmComparisontableApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BpmComparisontableApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/ems/bpm_comparisontable/BpmComparisontableApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ems.bpm_comparisontable;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BpmComparisontableApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
